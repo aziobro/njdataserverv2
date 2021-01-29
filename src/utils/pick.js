56a7e79b2203a2deecc34c1ca6a8ed000926c806
Andrew Ziobro <aziobro@gmail.com>
2021-01-29 10:05:48 -0500

    Fix Json Error

--- src/utils/pick.js
+++ src/utils/pick.js
@@ -8,9 +8,10 @@ const pick = (object, keys) => {
   return keys.reduce((obj, key) => {
     if (object && Object.prototype.hasOwnProperty.call(object, key)) {
       // eslint-disable-next-line no-param-reassign
-      console.log("---");
-      console.log(object[key]);
-      obj[key] = JSON.parse(object[key]);
+//      console.log("---");
+//      console.log(object[key]);
+      try{obj[key] = JSON.parse(object[key]); } catch(e) { obj[key] = object[key]; }
+      
     }
     return obj;
   }, {});
